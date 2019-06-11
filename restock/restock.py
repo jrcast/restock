8bc1b5f9c722f59879a4d844b45af336cc01fd61
Jhonattan Castillo <jrcast@users.noreply.github.com>
2019-06-10 23:23:05 -0700

    Initial release (#1)
    * Initial push of restock
    * add k8s manifests
    * update description in setup.py

---
+++ restock/restock.py
@@ -0,0 +1,66 @@
+#!flask/bin/python
+from collections import OrderedDict
+from flask import Flask, jsonify
+from flask import request as f_req
+import logging
+import os
+import requests
+
+
+class Restock():
+
+    def __init__(self):
+        self.healthy = False
+        self.query_url = "https://www.alphavantage.co/query"
+        self.logger = logging.getLogger("restock")
+        logformat = logging.Formatter('%(levelname)-8s %(message)s')
+        handler = logging.StreamHandler() #logging to stdout
+        handler.setFormatter(logformat)
+        self.logger.addHandler(handler)
+
+        self.app = Flask(__name__)
+        self.app.add_url_rule(rule='/', view_func=self.get_price, methods=['GET'])
+        self.app.add_url_rule(rule='/healthz', view_func=self.health_check, methods=['GET'])
+
+        self.api_key = os.environ.get("APIKEY", None)
+        if "APIKEY" is None:
+            self.logger.exception("APIKEY not found. Please define environ APIKEY")
+            raise Exception()
+
+        self.healthy = True
+
+    def get_price(self):
+        data = []
+        avg = 0
+        symbol = f_req.args.get('symbol', default=os.environ.get("SYMBOL", None), type=str)
+        ndays = f_req.args.get('ndays', default=int(os.environ.get('NDAYS', 4)), type=int) #if no ndays provided default to 4 days
+        qpayload = {"apikey": self.api_key, "function": "TIME_SERIES_DAILY_ADJUSTED", "symbol": symbol}
+        q = requests.get(self.query_url, params = qpayload)
+        try:
+            q.raise_for_status()
+            q = q.json(object_pairs_hook=OrderedDict) #Maintain order of elements
+
+            if 'Time Series (Daily)' not in q:
+                self.logger.error(q)
+                return jsonify({"success": False}), 500
+            else:
+                for day in list(q['Time Series (Daily)'].keys())[0:ndays]:
+                    data.append(float(q['Time Series (Daily)'][day]['4. close']))
+                    avg = sum(data) / len(data)
+        except Exception as e:
+            self.logger.error(e)
+        return jsonify({'success': True, 'symbol': symbol.upper(), 'ndays': ndays, "data": data, "avg": avg}), 200
+
+    def health_check(self):
+        if self.healthy:
+            return jsonify({"Health": "OK"}), 200
+        else:
+            return jsonify({"Health": "NOT_OK"}), 500
+
+
+def start_service():
+    restock = Restock()
+    return restock.app
+
+if __name__ == "__main__":
+    start_service().run(port = 10000, debug = True) #run directly to run debug mode ON
---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open('requirements.txt') as f:
+    reqs = f.read()
+
+setuptools.setup(name='restock',
+                 version="0.1.0",
+                 description="restock: Simple tool to quickly obtain stock information",
+                 author="jrcast",
+                 author_email="jrcast@users.noreply.github.com",
+                 packages=setuptools.find_packages(),
+                 include_package_data=True,
+                 url="https://github.com/jrcast/restock",
+                 install_requires = (reqs.strip().split("\n"),)
+                 )
